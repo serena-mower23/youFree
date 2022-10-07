e64e8dff4e14d5d00bbf16b19426243ac5d49d1e
mdsunray34 <60022435+mdsunray34@users.noreply.github.com>
2022-10-07 16:09:47 -0400

    Create initial page layout

---
+++ src/EventCalendar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { hot } from 'react-hot-loader/root';
+
+class EventCalendar extends React.Component {
+    render() {
+        return (
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title d-inline">{this.props.title}</h5>
+                    <div className="d-md-flex justify-content-md-end">
+                        <button className="btn btn-primary me-1">View</button>
+                        <button className="btn btn-danger">Delete</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default hot(EventCalendar);
