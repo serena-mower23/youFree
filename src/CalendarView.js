1acb90f4d012a04823e6ccf2c460a0e19addd682
Serena Mower <serena.mower23@gmail.com>
2022-10-10 18:11:52 -0400

    work with date-picker

--- src/CalendarView.js
+++ src/CalendarView.js
@@ -9,12 +9,35 @@ import Navbar from "./NavBar";
 import axios from 'axios';
 
 function datePicker() {
-    const [value, onChange] = useState(new Date());
-  
-     return (
-        <DatePicker onChange={onChange} value={value} />
-     );
-}
+    const defaultSelected = {
+        from: pastMonth,
+        to: addDays(pastMonth, 4)
+      };
+      const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+    
+      let footer = <p>Please pick the first day.</p>;
+      if (range?.from) {
+        if (!range.to) {
+          footer = <p>{format(range.from, 'PPP')}</p>;
+        } else if (range.to) {
+          footer = (
+            <p>
+              {format(range.from, 'PPP')}–{format(range.to, 'PPP')}
+            </p>
+          );
+        }
+      }
+    
+      return (
+        <DayPicker
+          mode="range"
+          defaultMonth={pastMonth}
+          selected={range}
+          footer={footer}
+          onSelect={setRange}
+        />
+      );
+    }
   
 let origSchedule = [new Date('2022-10-01 10:00:00'),
 new Date('2022-10-01 11:00:00'),
@@ -27,24 +50,28 @@ new Date('2022-10-02 14:00:00'),
 new Date('2022-10-02 15:00:00'),
 new Date('2022-10-02 16:00:00'),]
 
+let blankSchedule = []
+
 const values = [
     {label: "Blank Week", startDate: new Date("10-09-2022"), numDays: 7, dateFormat: "ddd"},
     {label: "Blank Work Week", startDate: new Date("10-10-2022"), numDays: 5, dateFormat: "ddd"},
     {label: "Custom Week", startDate: new Date("10-10-2022"), numDays: 5, dateFormat: "m/d"}
 ]
 
-const inputFile = document.querySelector('input')
+// const inputFile = document.querySelector('input')
 
-const convert = async (inputFile) => {
-    const icsRes = await fetch(inputFile)
-    const icsData = await icsRes.text()
-    // Convert
-    const data = icsToJson(icsData)
-    return data
-}
+// const convert = async (inputFile) => {
+//     const icsRes = await fetch(inputFile)
+//     const icsData = await icsRes.text()
+//     // Convert
+//     const data = icsToJson(icsData)
+//     return data
+// }
 
-const json = convert(inputFile)
-let dates = []
+// const json = convert(inputFile)
+// let dates = []
+
+// console.log(json);
 
 // json.forEach((element,index) => {
 //     let date = {
@@ -56,72 +83,98 @@ let dates = []
 
 
 class CalendarView extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            schedule: [],
+            otherSchedule: origSchedule,
+            selectedOption: null,
+            label: "Blank Week",
+            startDate: new Date("10-09-2022"),
+            numDays: 7,
+            dateFormat: "d/m",
+            //selectedFile: null,
+            dateValue: []
+        }
 
-    display = { 
-        selectedOption: null,
-        label: "Blank Week",
-        startDate: new Date("10-09-2022"),
-        numDays: 7,
-        dateFormat: "d/m"
-    };
+    // this.display = { 
+    //     selectedOption: null,
+    //     label: "Blank Week",
+    //     startDate: new Date("10-09-2022"),
+    //     numDays: 7,
+    //     dateFormat: "d/m"
+    // };
 
-    handleDisplay = (selectedOption) => {
+    // this.state = { schedule: [],
+    //     otherSchedule: origSchedule,
+    //  };
+
+    // this.file = {
+    //     selectedFile: null,
+    //   };
+
+      this.onFileChange = this.onFileChange.bind(this);
+      this.handleDisplay = this.handleDisplay.bind(this);
+      this.handleState = this.handleState.bind(this);
+      this.handleDateChange = this.handleDateChange(this);
+
+    }
+      
+    handleDisplay = selectedOption => {
         this.setState({ selectedOption: selectedOption });
         this.setState({ label: selectedOption.label})
         this.setState({ startDate: selectedOption.startDate });
         this.setState({ numDays: selectedOption.numDays });
         this.setState({ dateFormat: selectedOption.dateFormat });
-        console.log(selectedOption);
+        //console.log(selectedOption);
     };
-
-    // date = { date: new Date(),
-    //          numDays: 7}
-
-    // handleDate = 
-
-    state = { schedule: [],
-        otherSchedule: origSchedule };
 
     handleState = newSchedule => {
         this.setState({ schedule: newSchedule })
         this.setState({ otherSchedule: [...origSchedule, ...newSchedule]})
     }
 
-    file = {
- 
-        // Initially, no file is selected
-        selectedFile: null
-      };
-      
       // On file select (from the pop up)
       onFileChange = event => {
       
         // Update the state
-        this.setState({ selectedFile: event.target.files[0] });
-      
+        this.setState({ selectedFile: event.target.files[0]});
+        console.log("hello from the inside of onFileChange");
+        console.log(event.target.files[0]);
+        const icsRes = fetch(event.target.files[0])
+        const icsData = icsRes.text()
+            // Convert
+        const data = icsToJson(icsData)
+        this.console.log(data)
       };
+
+      handleDateChange = newDays => {
+        this.setState({ dateValue: newDays})
+      }
+    
       
       // On file upload (click the upload button)
       onFileUpload = () => {
       
         // Create an object of formData
         const formData = new FormData();
-        console.log("helloooo");
+
+        console.log("hello from the inside of onFileUpload");
       
         // Update the formData object
-        formData.append(
-          "myFile",
-          this.file.selectedFile,
-          this.file.selectedFile.name
-        );
+        // formData.append(
+        //   "myFile",
+        //   this.file.selectedFile,
+        //   this.file.selectedFile.name
+        // );
       
         // Details of the uploaded file
-        console.log("hello");
-        console.log(this.file.selectedFile);
+        // console.log("hello");
+        // console.log(this.file.selectedFile);
       
         // Request made to the backend api
         // Send formData object
-        axios.post("api/uploadfile", formData);
+        // axios.post("api/uploadfile", formData);
       };
       
       // File content to be displayed after
@@ -153,7 +206,6 @@ class CalendarView extends React.Component {
       
 
     render() {
-        const {selectedOption} = this.display
 
         return (
             <div>
@@ -163,7 +215,7 @@ class CalendarView extends React.Component {
                         <div className="col-md-3"></div>
                         <div className="col-md-6">
                             <Select
-                                value={selectedOption} 
+                                value={this.state.selectedOption} 
                                 options={values} 
                                 onChange={this.handleDisplay}
                             />
@@ -176,32 +228,36 @@ class CalendarView extends React.Component {
                         File Upload using React!
                     </h3>
                     <div>
-                        <input type="file" id="myFile" name="filename" display="none"/>
-                        {/* <input type="file" onChange={this.onFileChange} /> */}
+                        {/* <input type="file" id="myFile" name="filename" display="none"/> */}
+                        <input type="file" onChange={this.onFileChange} />
                         <button onClick={this.onFileUpload}>
                             Upload!
                         </button>
                     </div>
-                    {this.fileData()}
+                    {/* {this.fileData()} */}
                 </div>
 
                 <p>Select the times you're free below!</p>
-                {datePicker}
+                <DatePicker 
+                    returnValue={"range"}
+                    isOpen={true}
+                    onChange={this.handleDateChange} 
+                    value={this.state.dateValue} />
 
                 <div id="message">
                 <h3>My Availability</h3>
                 <p>Click and Drag to Toggle; Saved Immediately</p>
                 </div>
-                <div class="row justify-content-center">
-                <div class="col-6">
+                <div className="row justify-content-center">
+                <div className="col-6">
                 <ScheduleSelector
                     selection={this.state.schedule}
-                    startDate={this.display.startDate}
-                    numDays={this.display.numDays}
+                    startDate={this.state.startDate}
+                    numDays={this.state.numDays}
                     minTime={8}
                     maxTime={22}
                     hourlyChunks={4}
-                    dateFormat={this.display.dateFormat}
+                    dateFormat={this.state.dateFormat}
                     timeFormat={"h:mm a"}
                     unselectedColor={"#FA3D24"}
                     selectedColor={"rgba(80, 182, 51, 1)"}
@@ -209,10 +265,10 @@ class CalendarView extends React.Component {
                     onChange={this.handleState}
                     />
                 </div>
-                <div class="col-6">
+                {/* <div className="col-6">
                     <ScheduleSelector
                     selection={this.state.otherSchedule}
-                    startDate={new Date()}
+                    startDate={new Date('2022-10-01')}
                     numDays={7}
                     minTime={8}
                     maxTime={22}
@@ -222,7 +278,7 @@ class CalendarView extends React.Component {
                     selectedColor={"rgba(80, 182, 51, 1)"}
                     hoveredColor={"#ADB2AE"}
                     />
-                </div>
+                </div> */}
                 </div>
             </div>
         );
