7114c812e52218f265cb0d1fee2d8e18f6fdf75e
Serena Mower <serena.mower23@gmail.com>
2022-10-10 13:51:50 -0400

    edited navbar and added features to calendarview

--- src/CalendarView.js
+++ src/CalendarView.js
@@ -1,8 +1,11 @@
 import React, { useState} from "react";
 import ScheduleSelector from "react-schedule-selector";
-import DatePicker from 'react-date-picker'
-import 'react-date-picker/dist/DatePicker.css'
-import 'react-calendar/dist/Calendar.css'
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import { Link } from "react-router-dom";
+import Select from "react-select";
+import icsToJson from 'ics-to-json';
 
 function datePicker() {
     const [value, onChange] = useState(new Date());
@@ -14,21 +17,66 @@ function datePicker() {
   
 let origSchedule = []
 
+const values = [
+    {label: "Blank Week", startDate: new Date("10-09-2022"), numDays: 7, dateFormat: "ddd"},
+    {label: "Blank Work Week", startDate: new Date("10-10-2022"), numDays: 5, dateFormat: "ddd"},
+    {label: "Custom Week", startDate: new Date("10-10-2022"), numDays: 5, dateFormat: "m/d"}
+]
+
 class CalendarView extends React.Component {
+
+    display = { 
+        selectedOption: null,
+        label: "Blank Week",
+        startDate: new Date("10-09-2022"),
+        numDays: 7,
+        dateFormat: "d/m"
+    };
+
+    handleDisplay = (selectedOption) => {
+        this.setState({ selectedOption: selectedOption });
+        this.setState({ label: selectedOption.label})
+        this.setState({ startDate: selectedOption.startDate });
+        this.setState({ numDays: selectedOption.numDays });
+        this.setState({ dateFormat: selectedOption.dateFormat });
+        console.log(selectedOption);
+    };
+
+    // date = { date: new Date(),
+    //          numDays: 7}
+
+    // handleDate = 
+
     state = { schedule: [],
         otherSchedule: origSchedule };
 
-    handleChange = newSchedule => {
+    handleState = newSchedule => {
         this.setState({ schedule: newSchedule })
         this.setState({ otherSchedule: [...origSchedule, ...newSchedule]})
     }
 
     render() {
+        const {selectedOption} = this.display
+
         return (
             <div>
-                <h1>youFree?</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-3"></div>
+                        <div className="col-md-6">
+                            <Select
+                                value={selectedOption} 
+                                options={values} 
+                                onChange={this.handleDisplay}
+                            />
+                        </div>
+                        <div className="col-md-4"></div>
+                    </div>
+                </div>
+
                 <p>Select the times you're free below!</p>
                 {datePicker}
+
                 <div id="message">
                 <h3>My Availability</h3>
                 <p>Click and Drag to Toggle; Saved Immediately</p>
@@ -37,19 +85,20 @@ class CalendarView extends React.Component {
                 <div class="col-6">
                 <ScheduleSelector
                     selection={this.state.schedule}
-                    startDate={new Date()}
-                    numDays={7}
+                    startDate={this.display.startDate}
+                    numDays={this.display.numDays}
                     minTime={8}
                     maxTime={22}
                     hourlyChunks={4}
+                    dateFormat={this.display.dateFormat}
                     timeFormat={"h:mm a"}
                     unselectedColor={"#FA3D24"}
                     selectedColor={"rgba(80, 182, 51, 1)"}
                     hoveredColor={"#ADB2AE"}
-                    onChange={this.handleChange}
+                    onChange={this.handleState}
                     />
                 </div>
-                <div class="col-6">
+                {/* <div class="col-6">
                     <ScheduleSelector
                     selection={this.state.otherSchedule}
                     startDate={new Date()}
@@ -62,7 +111,7 @@ class CalendarView extends React.Component {
                     selectedColor={"rgba(80, 182, 51, 1)"}
                     hoveredColor={"#ADB2AE"}
                     />
-                </div>
+                </div> */}
                 </div>
             </div>
         );
