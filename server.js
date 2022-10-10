355ac86a34b904e63d96fd76b0be48ed3d0083aa
fmcshan <fmcshan@wpi.edu>
2022-10-10 16:57:22 -0400

    add login page, add mongodb

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require( 'express' ),
       mongodb = require( 'mongodb' ),
       app     = express()
+      cookie = require( 'cookie-session' )
+      // httpProxy = require('http-proxy');
+      // proxy = httpProxy.createServer({});
     //   hbs     = require( 'express-handlebars' ).engine,
     //   config = require('./src/config.js'),
     //   cookieParser = require('cookie-parser'),
@@ -28,13 +31,17 @@ app.use(express.urlencoded({ extended : true }));
 
 
 //Database connection
-const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = "mongodb+srv://"+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@"+process.env.HOST+"/?retryWrites=true&w=majority";
-const client = new MongoClient( uri, { useNewURLParser: true, useUnifiedTopology:true, serverApi: ServerApiVersion.v1 });
+const client = new mongodb.MongoClient( uri, { useNewURLParser: true, useUnifiedTopology:true });
 let collection = null;
 
 
 //connect to database and grab collection
+app.use( cookie({
+  name: 'session',
+  keys: ['key934537823', 'key974123823']
+}))
+
 client.connect() 
   .then( () => {
     return client.db( 'test' ).collection( 'test' )
@@ -42,6 +49,35 @@ client.connect()
   .then( __collection => {
     collection = __collection
     return collection.find({ }).toArray()
+})
+
+app.post( '/login', (req,res)=> {
+  let seen = false
+
+  client.db( 'test' ).collection( 'test' ).find().forEach(element => {
+    collection.find({username: req.body.username})
+    .toArray()
+    .then(result => {
+      req.session.username = req.body.username
+      req.session.password = req.body.password
+
+      if (result.length > 0) {
+        if (req.body.password === result[0].password) {
+          req.session.login = true
+          //redirect to home page?
+        } else {
+          //stay at login page
+        }
+      } else {
+        req.body.data = []
+        collection.insertOne( req.body ).then(result => {
+          //proxy.web(req, res, { target: 'http://localhost:8080/home' });
+        })
+
+      }
+    })
+
+  })
 })
 
 app.get('/', function(req, res) {
--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,25 @@ class Login extends React.Component {
     render() {
         return (
             <div>
-                <h1>Login Page</h1>
+                <div class="container mt-5 d-flex flex-column align-items-center">
+                    <h1>youFree?</h1>
+                    <p>Please log in to create and manage your calendars.</p>
+                    <form class="needs-validation" action="/login" method="POST" novalidate>
+                        <div class="mb-3">
+                            <label class="form-label" for="username">Username:</label>
+                            <input class="form-control" type="text" name="username" id="username" required/>
+                            <div class="invalid-feedback">Please provide a username.</div> 
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="password">Password:</label>
+                            <input class="form-control" type="password" name="password" id="password" required/>
+                            <div class="invalid-feedback">Please provide a password.</div> 
+                        </div>
+                        <div class="d-grid d-sm-block text-center">
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
