da2eecd799c60cd3f21470978c84f19c7dc04376
mdsunray34 <60022435+mdsunray34@users.noreply.github.com>
2022-10-10 23:24:12 -0400

    Implement login and logout

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const express = require( 'express' ),
       mongodb = require( 'mongodb' ),
-      app     = express()
+      app     = express(),
       cookie = require( 'cookie-session' )
-      // httpProxy = require('http-proxy');
-      // proxy = httpProxy.createServer({});
-    //   hbs     = require( 'express-handlebars' ).engine,
-    //   config = require('./src/config.js'),
-    //   cookieParser = require('cookie-parser'),
-      ////expressSession = require('express-session'),
-    //   methodOverride = require('method-override');
+
+require('dotenv').config()
 
 app.use( express.json() )
-
 app.use( express.static( 'dist' ) )
 app.use( express.static( 'src' ) )
-require('dotenv').config()
+app.use(express.urlencoded({ extended : true }));
 
 // app.engine( 'handlebars',  hbs() )
 // app.set(    'view engine', 'handlebars' )
-app.set(    'views',       './dist' )
+// app.set(    'views',       './dist' )
 
 // app.set('view engine', 'html');
 // app.set('views', __dirname + '/views');
@@ -27,14 +21,10 @@ app.set(    'views',       './dist' )
 // app.use(methodOverride());
 // app.use(cookieParser());
 
-app.use(express.urlencoded({ extended : true }));
-
-
 //Database connection
 const uri = "mongodb+srv://"+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@"+process.env.HOST+"/?retryWrites=true&w=majority";
 const client = new mongodb.MongoClient( uri, { useNewURLParser: true, useUnifiedTopology:true });
 let collection = null;
-
 
 //connect to database and grab collection
 app.use( cookie({
@@ -51,44 +41,51 @@ client.connect()
     return collection.find({ }).toArray()
 })
 
-app.post( '/login', (req,res)=> {
-  let seen = false
+app.post('/login', (req, res) => {
+  collection.find({username: req.body.username})
+  .toArray()
+  .then(result => {
+    req.session.username = req.body.username
+    req.session.password = req.body.password
 
-  client.db( 'test' ).collection( 'test' ).find().forEach(element => {
-    collection.find({username: req.body.username})
-    .toArray()
-    .then(result => {
-      req.session.username = req.body.username
-      req.session.password = req.body.password
-
-      if (result.length > 0) {
-        if (req.body.password === result[0].password) {
-          req.session.login = true
-          //redirect to home page?
-        } else {
-          //stay at login page
-        }
+    if (result.length > 0) {
+      if (req.body.password === result[0].password) {
+        req.session.login = true
+        // redirect to home page
+        res.redirect('http://localhost:8080/home')
       } else {
-        req.body.data = []
-        collection.insertOne( req.body ).then(result => {
-          //proxy.web(req, res, { target: 'http://localhost:8080/home' });
-        })
-
+        // stay at login page
+        res.redirect('http://localhost:8080')
       }
-    })
-
+    } else {
+      req.body.data = []
+      collection.insertOne( req.body )
+      res.redirect('http://localhost:8080/home')
+    }
   })
 })
 
-app.get('/', function(req, res) {
-    res.render('index');
-  });
+app.post('/logout', (req, res, next) => {
+  req.session.login = false
+  next()
+})
+
+app.use( function(req, res, next) {
+  if ( req.session.login === true ) {
+    next()
+  } else {
+    res.redirect('http://localhost:8080')
+  }
+})
+
+// app.get('/', function(req, res) {
+//     res.render('index');
+//   });
 
 app.get('/*', function(req, res) {
     res.sendFile(__dirname + '/dist/index.html', function(err) {
         res.status(500).send(err)
     })
 })
-  
 
 app.listen( 8080 )
--- src/Login.js
+++ src/Login.js
@@ -1,25 +1,36 @@
 import React from "react";
 
+window.onload = function() {
+    const loginForm = document.querySelector(".needs-validation")
+    loginForm.addEventListener("submit", event => {
+        if (!loginForm.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            loginForm.classList.add("was-validated")
+        }
+    })
+}
+
 class Login extends React.Component {
     render() {
         return (
             <div>
-                <div class="container mt-5 d-flex flex-column align-items-center">
+                <div className="container mt-5 d-flex flex-column align-items-center">
                     <h1>youFree?</h1>
                     <p>Please log in to create and manage your calendars.</p>
-                    <form class="needs-validation" action="/login" method="POST" novalidate>
-                        <div class="mb-3">
-                            <label class="form-label" for="username">Username:</label>
-                            <input class="form-control" type="text" name="username" id="username" required/>
-                            <div class="invalid-feedback">Please provide a username.</div> 
+                    <form className="needs-validation" action="/login" method="POST" noValidate>
+                        <div className="mb-3">
+                            <label className="form-label" htmlFor="username">Username:</label>
+                            <input className="form-control" type="text" name="username" id="username" required/>
+                            <div className="invalid-feedback">Please provide a username.</div> 
                         </div>
-                        <div class="mb-3">
-                            <label class="form-label" for="password">Password:</label>
-                            <input class="form-control" type="password" name="password" id="password" required/>
-                            <div class="invalid-feedback">Please provide a password.</div> 
+                        <div className="mb-3">
+                            <label className="form-label" htmlFor="password">Password:</label>
+                            <input className="form-control" type="password" name="password" id="password" required/>
+                            <div className="invalid-feedback">Please provide a password.</div> 
                         </div>
-                        <div class="d-grid d-sm-block text-center">
-                            <button type="submit" class="btn btn-primary">Login</button>
+                        <div className="d-grid d-sm-block text-center">
+                            <button type="submit" className="btn btn-primary">Login</button>
                         </div>
                     </form>
                 </div>
