7d299211df99129f5884e3666d76702b8184629b
fmcshan <fmcshan@wpi.edu>
2022-10-14 16:50:14 -0400

    fix styling, add link to website title

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,9 @@ function Navbar() {
     <div className="container">
         <nav className="navbar navbar-expand-lg mb-4">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">youFree?</a>
+                <Link className="navbar-brand" to='/home'>
+                    youFree?
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
